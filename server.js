72984407b78f2ee43e147c5e43436b6e864986d0
Steven Israel <malenchite@gmail.com>
2021-03-13 11:38:55 -0500

    Travis test

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ require('./config/passport')(db, app, passport); // pass passport for configurat
 
 // Define our routes
 app.use('/api', require('./routes/apiRoutes')(passport, db));
-app.use(require('./routes/htmlRoutes')(db));
+app.use(require('./routes/htmlRoutes')(db))
 
 // Secure express app
 app.use(helmet.hsts({
