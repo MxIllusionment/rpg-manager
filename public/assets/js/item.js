2583811dd651acce3ad4ad1a2b3c1428b91b77d2
Steven Israel <malenchite@gmail.com>
2021-03-20 11:30:13 -0400

    Adds load-current functionality to character and item lists

--- public/assets/js/item.js
+++ public/assets/js/item.js
@@ -33,22 +33,39 @@ const refreshItemList = function () {
     itemList.append(items);
 
     $('.item-btn').click(function () {
-      $('.item-btn').removeClass('selected-item-btn');
-      $(this).addClass('selected-item-btn');
-      selectItem($(this).attr('data-index'));
+      selectItem($(this));
       sessionStorage.setItem('ItemId', JSON.stringify($(this).attr('data-id')));
     });
   });
 };
 
-// Loads the selected item into the right column
-const selectItem = index => {
+// Loads the selected item into the right column and highlights the list item
+const selectItem = li => {
+  const index = li.attr('data-index');
+
+  $('.item-btn').removeClass('selected-item-btn');
+  li.addClass('selected-item-btn');
+
   if (itemData.length > 0) {
     itemName.text(itemData[index].name);
     itemDesc.text(decodeURI(itemData[index].description));
   } else {
     itemName.empty();
     itemDesc.empty();
+  }
+};
+
+// Loads the current item from the session
+const loadCurrentItem = () => {
+  const currentItem = parseInt(JSON.parse(sessionStorage.getItem('ItemId')));
+
+  if (currentItem) {
+    // Find character entry in data
+    const li = itemList.find(`[data-id=${currentItem}]`);
+
+    if (li !== -1) {
+      selectItem(li);
+    }
   }
 };
 
@@ -61,4 +78,5 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip();
 });
 
-refreshItemList();
+refreshItemList()
+  .then(loadCurrentItem);
